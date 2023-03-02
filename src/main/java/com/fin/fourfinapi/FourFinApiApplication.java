eb2b32bb9047ea69f898c5a48e239aec310152d2
pertusatti22 <pertusatti22@gmail.com>
2023-03-02 15:39:25 -0300

    Initial commit

---
+++ src/main/java/com/fin/fourfinapi/FourFinApiApplication.java
@@ -0,0 +1,13 @@
+package com.fin.fourfinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourFinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourFinApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fin/fourfinapi/FourFinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin.fourfinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourFinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
