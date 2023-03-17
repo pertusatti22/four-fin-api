dc1c1807c603192d07262d7b444237c2d4e8054e
pertusatti22 <pertusatti22@gmail.com>
2023-03-17 15:56:48 -0300

    Add samples

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into conta (nome, valor_inicial) values ('Banco do Brasil', 5000);
+insert into conta (nome, valor_inicial) values ('SICREDI', 3000);
+insert into conta (nome, valor_inicial) values ('NuBank', 150);
+
+insert into categoria (nome) values ('Alimentação');
+insert into categoria (nome) values ('Transporte');
