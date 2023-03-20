848747c8b60e93ae7745757faa16ac22a9ce446f
pertusatti22 <pertusatti22@gmail.com>
2023-03-19 22:33:23 -0300

    updt controllers

--- src/main/java/com/fin/fourfinapi/api/controller/CategoriaController.java
+++ src/main/java/com/fin/fourfinapi/api/controller/CategoriaController.java
@@ -2,6 +2,8 @@ package com.fin.fourfinapi.api.controller;
 
 import com.fin.fourfinapi.domain.model.Categoria;
 import com.fin.fourfinapi.domain.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,10 @@ import java.util.List;
 @RequestMapping("/categorias")
 public class CategoriaController {
 
+    @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.listar();
     }
--- src/main/java/com/fin/fourfinapi/api/controller/TransacaoController.java
+++ src/main/java/com/fin/fourfinapi/api/controller/TransacaoController.java
@@ -3,6 +3,7 @@ package com.fin.fourfinapi.api.controller;
 import com.fin.fourfinapi.domain.model.Transacao;
 import com.fin.fourfinapi.domain.repository.TransacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class TransacaoController {
     @Autowired
     private TransacaoRepository transacaoRepository;
 
+    @GetMapping
     public List<Transacao> listar() {
         return transacaoRepository.listar();
     }
