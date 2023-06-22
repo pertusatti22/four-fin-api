976256133c06a81fe7fe28aab998542b1b43ebe0
pertusatti22 <pertusatti22@gmail.com>
2023-06-21 22:43:38 -0300

    Criando gets específicos

--- src/main/java/com/fin/fourfinapi/api/controller/TransacaoController.java
+++ src/main/java/com/fin/fourfinapi/api/controller/TransacaoController.java
@@ -14,6 +14,7 @@ import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -31,6 +32,17 @@ public class TransacaoController {
     @GetMapping
     public List<Transacao> listar() {
         return transacaoRepository.findAll();
+    }
+
+    @GetMapping("/anotacao")
+    public List<Transacao> listarPorAnotacao(@RequestParam("anotacao") String anotacao) {
+        return transacaoRepository.findAllByAnotacaoContaining(anotacao);
+    }
+
+    @GetMapping("/valor")
+    public List<Transacao> transacoesPorValor(
+            BigDecimal valorInicial, BigDecimal valorFinal) {
+        return transacaoRepository.findAllByValorBetween(valorInicial, valorFinal);
     }
 
     @GetMapping("/{transacaoId}")
--- src/main/java/com/fin/fourfinapi/domain/repository/TransacaoRepository.java
+++ src/main/java/com/fin/fourfinapi/domain/repository/TransacaoRepository.java
@@ -4,8 +4,12 @@ import com.fin.fourfinapi.domain.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 @Repository
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
 
+    List<Transacao> findAllByAnotacaoContaining(String anotacao);
+
+    List<Transacao> findAllByValorBetween(BigDecimal valorInicial, BigDecimal valorFinal);
 }
